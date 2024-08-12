d8bd1d243a08fd4e2ecedaa133687a3c48d02ff4
Yohann Pirbay <pirbay.yohann@gmail.com>
2024-08-12 17:57:38 +0400

    refactor and comment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import secrets
+# We use secrets instead of random as it is preferred when generating passwords
+import secrets 
 
+# Character sets
 lowercase_chars = 'abcdefghijklmnopqrstuvwxyz'
 uppercase_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numbers = '0123456789'
@@ -7,6 +9,7 @@ special_chars = '~`!@#$%^&*()_-+={[}]|\\:;\"\'<,>.?/'
 
 chars = lowercase_chars + uppercase_chars + numbers + special_chars
 
+# Generates a password of given length
 def generate_password(length):
 
     password = [secrets.choice(lowercase_chars),
@@ -14,56 +17,55 @@ def generate_password(length):
                 secrets.choice(numbers),
                 secrets.choice(special_chars)]
     
-    for i in range(length - 4):
+    for i in range(length - len(password)):
         password.append(secrets.choice(chars))
     
     secrets.SystemRandom().shuffle(password)
 
     return ''.join(password)
 
+# Gets user input for the wanted length of the password
 def get_length():
     while True:
         try:
             length = int(input("How long would you like your password to be: "))
             print()
             if length < 0:
-                print("Please enter a positive number!")
-                print()
+                print("Please enter a positive number!\n")
             elif length < 8:
-                print("For a secure password, you must have at least 8 characters!")
-                print()
+                print("For a secure password, you must have at least 8 characters!\n")
             else:
                 return length
         except ValueError:
-            print()
-            print("Please input a number!")
-            print()
+            print("\nPlease input a number!\n")
         except:
-            print("Something went wrong, please try again!")
+            print("\nSomething went wrong, please try again!\n")
 
+# Displays a welcome message
 def welcome():
     print("-" * 55)
     print("Welcome to your personal and secure password generator!")
-    print("-" * 55)
-    print()
+    print("-" * 55 + "\n")
 
+# Displays the generated password
 def display_password(length):
-    print("Here is your password: " + generate_password(length))
-    print()
+    print("Here is your password: " + generate_password(length) + "\n")
 
+# Asks if the user wants to generate a new password
 def regenerate():
     while True:
         answer = input("Would you like to generate a new password (y/n)? ").lower()
         print()
         if answer not in ['y', 'n']:
-            print("Please chose between 'y' or 'n'!")
-            print()
+            print("Please chose between 'y' or 'n'!\n")
         else:
             return answer == 'y'
-        
-def end_message():
-    print("Thanks for using our tool :)")
 
+# Displays an end message  
+def end_message():
+    print("Thanks for using my tool :)")
+
+# Main function to start the program
 def start():
     welcome()
     while True:
